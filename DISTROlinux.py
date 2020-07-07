7a950a1765f69073b22d4c908d3b462afae57f15
INDONESIANtrack5 <67874169+INDONESIANtrack5@users.noreply.github.com>
2020-07-08 00:44:04 +0700

    Create DISTROlinux.py

---
+++ DISTROlinux.py
@@ -0,0 +1,60 @@
+import os
+
+#jangan di recode Bro berkaryalah sendiri ini secript sederhana
+
+os.system('clear')
+os.system('sleep 3')
+
+print('\033[1;30m[]========DISTRO LINUX=========[]\033[1;30m ')
+print('VERSI  : 0.3')
+print('INDONESIAN TRACK5')
+print('ATHOR BY : MR  Ebeck kansas Real')
+print('\033[0;36m1.LINUX \033[0;36m ')
+print('2.DEBIAN')
+print('3.ANDROID')
+print('4.UBUNTU')
+print('5.FUDUNTU')
+print('6.KALI LINUX')
+print('7.GOOGLE CRHOME')
+print('8.ARCO LINUX')
+print('9.LINUX MIN')
+print('10.CARI LOGO SENDIRI')
+print('\033[0;36m11.EXIT \033[0;36m ')
+insta=input('\033[1;31m[]=\033[1;31m==> ')
+
+
+if insta == '1':
+  os.system('apt install screenfetch')
+  os.system('screenfetch -A Linux')
+elif insta == '2':
+    os.system('apt install screenfetch')
+    os.system('screenfetch -A Debian')
+elif insta == '3':
+    os.system('apt install screenfetch')
+    os.system('screenfetch')
+elif insta == '4':
+    os.system('apt install screenfetch')
+    os.system('screenfetch -A Ubuntu')
+elif insta == '5':
+    os.system('apt install screenfetch')
+elif insta == '6':
+    os.system('apt install screenfetch')
+    os.system('screenfetch -A "Kali Linux" ')
+elif insta == '7':
+    os.system('apt install screenfetch')
+    os.system('screenfetch -A "Chrome OS" ')
+elif insta == '8':
+    os.system('apt install screenfetch')
+    os.system('screenfetch -A ArcoLinux')
+elif insta == '9':
+    os.system('apt install screenfetch')
+    os.system('screenfetch -A "Linux Mint" ')
+elif insta == '10':
+    os.system('apt install screenfetch')
+    os.system('screenfetch --help')
+elif insta == '11':
+    os.system('exit')
+print('terimakasih udah menggunakan tools ini')
+print('ATHOR : MR EBECK KANSAS REAL')
+print('github: https://github.com/INDONESIANtrack5')
+print('facebook: Indonesian Track5')
